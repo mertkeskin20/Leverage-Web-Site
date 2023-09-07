3e52c3cb5a9241213bd3250ce90b67a5865a08cb
Mert Keskin <keskinnos09@gmail.com>
2023-09-07 20:02:17 +0300

    Leverage-Web-Site
    HTML-CSS-JS

---
+++ leverage/js/navbar.js
@@ -0,0 +1,7 @@
+let menuToggle = document.querySelector('.menuToggle');
+
+let header = document.querySelector('header');
+
+menuToggle.onclick = function(){
+    header.classList.toggle('active');
+}
